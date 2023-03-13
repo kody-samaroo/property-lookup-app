ff68c9fc9e116bd7fed0250be17766e9dadce660
Kody Samaroo <kody.samaroo@gmail.com>
2023-03-13 19:39:01 -0400

    property photo to the show pages

--- app/helpers/properties_helper.rb
+++ app/helpers/properties_helper.rb
@@ -1,9 +1,12 @@
 module PropertiesHelper
 
-
     def property_thumbnail property
         img = property.photo.present? ? property.photo.thumb.url : "house.jpeg"
         image_tag img, class: "property-thumb", width: "100%", height: "225px"
+    end
+
+    def property_photo property
+        img = property.photo.present? ? property.photo.thumb.url : asset_url("house.jpeg")
     end
 
 end
