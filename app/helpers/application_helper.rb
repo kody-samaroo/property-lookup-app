b94c03d23ab89581fc2193b791e94bfa6c5e8c72
Kody Samaroo <kody.samaroo@gmail.com>
2023-03-15 17:19:46 -0400

    tidying up some styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+    def profile_picture account
+        thumb = account.image.present? ? account.image.thumbnail.url : "profile.png"
+        image_tag thumb, class: "profile-picture img-circle", style: "border-radius: 30%; width: 100px;"
+    end
 end
