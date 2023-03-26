da54cd8289aab84a84ffa7d1c95fc10b9ca0a523
Kody Samaroo <kody.samaroo@gmail.com>
2023-03-25 20:32:38 -0400

    displaying announcements, need styling

--- app/helpers/news_helper.rb
+++ app/helpers/news_helper.rb
@@ -1,2 +1,15 @@
 module NewsHelper
+
+    def find_author new
+        first = Account.where(id: new.account_id)[0].first_name
+        last = Account.where(id: new.account_id)[0].last_name
+        author = first + " " + last
+    end
+
+    def find_date new
+        day = new.created_at.strftime("%A")
+        month = new.created_at.strftime("%B")
+        date = new.created_at.day.ordinalize
+        show_datetime = "#{day}, #{month} #{date}"
+    end
 end
