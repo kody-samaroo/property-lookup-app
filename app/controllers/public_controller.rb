a2dfeb103e6bd4b65b25872c8063904486a59f36
Kody Samaroo <kody.samaroo@gmail.com>
2023-04-07 16:05:56 -0400

    fixed add property button

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,5 +1,14 @@
 class PublicController < ApplicationController
   def main
+    # if account_signed_in?
+    #   redirect_to root_path, flash: { success: "Successfully signed in. Welcome to Fast Homes" } and return
+    # end
+
+    # if account_signed_in?
+    #   path = current_account.admin? ? accounts_path : dashboard_path
+    #   redirect_to path, flash: { success: "Successfully signed in. Welcome to Fast Homes" } and return
+    # end
+
     @properties = Property.latest.paginate(page: params[:page], per_page: 17)
     @news = News.all
   end
